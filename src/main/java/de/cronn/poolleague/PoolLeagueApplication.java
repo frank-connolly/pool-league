2d9b31bbb3763977afa414d84e708a473dc20952
Frank Connolly <fconnolly1@gmail.com>
2022-10-30 02:32:33 +0200

    Spring Initializr skeleton

---
+++ src/main/java/de/cronn/poolleague/PoolLeagueApplication.java
@@ -0,0 +1,13 @@
+package de.cronn.poolleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoolLeagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/cronn/poolleague/PoolLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cronn.poolleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolLeagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
