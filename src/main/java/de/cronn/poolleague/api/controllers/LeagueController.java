4b292a28ac706d3f4f97de6ecba0ed289e7c0a05
Frank Connolly <fconnolly1@gmail.com>
2022-10-31 01:24:25 +0100

    Updated all endpoints to use ResponseEntities

--- src/main/java/de/cronn/poolleague/api/controllers/LeagueController.java
+++ src/main/java/de/cronn/poolleague/api/controllers/LeagueController.java
@@ -3,13 +3,15 @@ package de.cronn.poolleague.api.controllers;
 import de.cronn.poolleague.model.League;
 import de.cronn.poolleague.repositories.LeagueRepository;
 import de.cronn.poolleague.repositories.PlayerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/leagues")
@@ -22,13 +24,16 @@ public class LeagueController {
     }
 
     @GetMapping
-    public List<League> getLeagues() {
-        return leagueRepository.findAll();
+    public ResponseEntity<List<League>> getLeagues() {
+        List<League> leagues = leagueRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(leagues);
     }
 
     @GetMapping("/{leagueId}")
-    public League getLeagueById(@PathVariable Long leagueId) {
-        return leagueRepository.findById(leagueId)
-                .orElseThrow(() -> new NoSuchElementException("League with id " + leagueId + " not found."));
+    public ResponseEntity<Optional<League>> getLeagueById(@PathVariable Long leagueId) {
+        if (leagueRepository.existsById(leagueId)) {
+            return ResponseEntity.status(HttpStatus.OK).body(leagueRepository.findById(leagueId));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
--- src/main/java/de/cronn/poolleague/api/controllers/PlayerController.java
+++ src/main/java/de/cronn/poolleague/api/controllers/PlayerController.java
@@ -6,15 +6,20 @@ import de.cronn.poolleague.model.League;
 import de.cronn.poolleague.model.Player;
 import de.cronn.poolleague.repositories.LeagueRepository;
 import de.cronn.poolleague.repositories.PlayerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.bind.ValidationException;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
+
+    // TODO Either fill in bad responses with specific error info on why it's wrong OR
+    // refactor bad responses to throw exceptions instead and catch in a separate error handling class
 
     private PlayerRepository playerRepository;
     private LeagueRepository leagueRepository;
@@ -25,43 +30,55 @@ public class PlayerController {
     }
 
     @GetMapping
-    public List<Player> getPlayers() {
-        return playerRepository.findAll();
+    public ResponseEntity<List<Player>> getPlayers() {
+        List<Player> players = playerRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(players);
     }
 
     @GetMapping("/{playerId}")
-    public Player getPlayerById(@PathVariable Long playerId) {
-        return playerRepository.findById(playerId)
-                .orElseThrow(() -> new NoSuchElementException("No player with id " + playerId + " found."));
+    public ResponseEntity<Optional<Player>> getPlayerById(@PathVariable final Long playerId) {
+        if (playerRepository.existsById(playerId)) {
+            return ResponseEntity.status(HttpStatus.OK).body(playerRepository.findById(playerId));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @PostMapping
-    public Player addPlayer(@RequestBody AddPlayerRequest playerRequest) {
+    public ResponseEntity<Player> addPlayer(@RequestBody AddPlayerRequest playerRequest) {
         Player player = new Player();
         player.setFirstName(playerRequest.getFirstName());
         player.setLastName(playerRequest.getLastName());
-        return playerRepository.save(player);
+        playerRepository.save(player);
+        return ResponseEntity.status(HttpStatus.CREATED).body(player);
     }
 
     @PutMapping("/{playerId}")
-    public void addPlayerToLeague(@PathVariable Long playerId, @RequestBody JoinLeagueRequest joinLeagueRequest) {
-        Player player = playerRepository.findById(playerId)
-                .orElseThrow(() -> new NoSuchElementException("No player with id " + playerId + " found."));
-        League league = leagueRepository.findById(joinLeagueRequest.getLeagueId())
-                .orElseThrow(() -> new NoSuchElementException("Requested league not found"));
-
-        if (joinLeagueRequest.getLeagueCode().equals(league.getCode())) {
-            player.setLeagueId(joinLeagueRequest.getLeagueId());
-            playerRepository.save(player);
-        } else {
-            System.out.println("League code incorrect");
+    public ResponseEntity<Player> addPlayerToLeague(@PathVariable Long playerId,
+                                                              @RequestBody JoinLeagueRequest request) {
+        if (!playerRepository.existsById(playerId) || !leagueRepository.existsById(request.getLeagueId())) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+        Optional<Player> player = playerRepository.findById(playerId);
+        if (isValidLeagueCode(request.getLeagueId(), request.getLeagueCode())) {
+            player.get().setLeagueId(request.getLeagueId());
+            return ResponseEntity.status(HttpStatus.OK).body(playerRepository.save(player.get()));
+        }
+        System.out.println("League code incorrect");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
+    public boolean isValidLeagueCode(Long leagueId, String code) {
+        League league = leagueRepository.findById(leagueId).orElseThrow(NoSuchElementException::new);
+        return league.getCode().equals(code);
+    }
+
+    // TODO Add security to delete endpoint
     @DeleteMapping("/{playerId}")
-    public void deletePlayer(@PathVariable Long playerId) {
+    public ResponseEntity deletePlayer(@PathVariable Long playerId) {
         Player player = playerRepository.findById(playerId)
                 .orElseThrow(() -> new NoSuchElementException("No player with id " + playerId + " found."));
         playerRepository.delete(player);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
     }
 }
